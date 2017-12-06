8a31e133c511757b9c4c876a9946dea2a0bf31ea
Henry Yang <henryyang0217@gmail.com>
2017-12-05 16:54:13 -0800

    Add edit function to "role"

--- src/main/java/com/hanyang/instateam/web/controller/RoleController.java
+++ src/main/java/com/hanyang/instateam/web/controller/RoleController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -26,15 +28,35 @@ public class RoleController {
     return "roles";
   }
 
+  @RequestMapping("editrole/{roleId}")
+  public String formEditRole(@PathVariable Long roleId, Model model) {
+    Role role = roleService.findById(roleId);
+    model.addAttribute("role", role);
+    return "edit_role";
+  }
+
   @RequestMapping(value = "/roles", method = RequestMethod.POST)
   public String addRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
     //System.out.println(role.getName());
     if (result.hasErrors()) {
       //System.out.println("enter");
-      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.category", result);
+      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.role", result);
       redirectAttributes.addFlashAttribute("role", role);
 
       return "redirect:/roles";
+    }
+
+    roleService.save(role);
+    return "redirect:/roles";
+  }
+
+  @RequestMapping(value = "/editrole/{roleId}", method = RequestMethod.POST)
+  public String editRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
+    if (result.hasErrors()) {
+      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.role", result);
+      redirectAttributes.addFlashAttribute("role", role);
+
+      return String.format("redirect:/editrole/%s", role.getId());
     }
 
     roleService.save(role);
